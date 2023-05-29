c58615f9e0d7fa4645ae3a8398c36ecbe1f38e10
hasansp22 <hasan.afandi.sp@gmail.com>
2023-05-29 22:44:03 +0700

    config 3

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize("cloud_prakt_db", "root", "G!m5*hb8Hj4$", {
-  host: "dbmysql-be-cloud-1.cni74cuasoyo.ap-southeast-1.rds.amazonaws.com:3306",
+  host: "dbmysql-be-cloud-1.cni74cuasoyo.ap-southeast-1.rds.amazonaws.com",
   dialect: "mysql",
 });
 
